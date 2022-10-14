b933e3f1d659c92309766a9caa5ffd3cbb468616
diapmusic <diapmusic01@gmail.com>
2022-10-14 21:32:06 +0100

    main

---
+++ c#-Unity/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class Camera_Follow : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+    private Vector3 newtrans;
+
+    void Start ()
+    {
+        offset.x = transform.position.x - player.transform.position.x;
+        offset.z = transform.position.z - player.transform.position.z;
+        newtrans=transform.position;
+    //not taking y as we won't update y position. 
+
+    }
+    void LateUpdate ()
+    {
+        newtrans.x= player.transform.position.x + offset.x;
+        newtrans.z= player.transform.position.z + offset.z;
+        transform.position = newtrans;
+    }
+
+}
