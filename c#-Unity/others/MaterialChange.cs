2432d6d836f9494ad72a13762f679ba56a8bdeba
diapmusic <diapmusic01@gmail.com>
2022-10-30 09:08:51 +0100

    main

--- c#-Unity/others/MaterialChange.cs
+++ c#-Unity/others/MaterialChange.cs
@@ -14,7 +14,7 @@ public class MaterialChange : MonoBehaviour
     Renderer rend;
     public bool matActive;
 
-    public GameObject RBOtoNBO;
+    public GameObject Character;
     // Start is called before the first frame update
     void Start()
     {
