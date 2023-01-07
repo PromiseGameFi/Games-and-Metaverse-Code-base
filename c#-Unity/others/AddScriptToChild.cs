1c7ae2d8ced0d9a274e0fb19d8e21554e805234f
Promise Emmanuel <95595405+Promise-csharp@users.noreply.github.com>
2023-01-07 04:08:28 +0100

    Create AddScriptToChild.cs

---
+++ c#-Unity/others/AddScriptToChild.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AddScriptToColliders : MonoBehaviour
+{
+    public MonoBehaviour scriptToAdd;
+
+    void Start()
+    {
+        // Get all the colliders on the children of this GameObject
+        Collider[] colliders = transform.GetComponentsInChildren<Collider>();
+
+        // Iterate through the colliders
+        foreach (Collider collider in colliders)
+        {
+            // Add the script to the GameObject with the collider
+            collider.gameObject.AddComponent(scriptToAdd.GetType());
+        }
+    }
+}
